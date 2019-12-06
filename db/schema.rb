00f63ed7947026536b45540f60ceb3d1af4f983b
David <dafrogriguezro@unal.edu.co>
2019-12-06 16:22:44 -0500

    :fire: session fixed

--- db/schema.rb
+++ db/schema.rb
@@ -71,6 +71,11 @@ ActiveRecord::Schema.define(version: 2019_12_05_230750) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
     t.string "name"
     t.string "nickname"
     t.string "image"
